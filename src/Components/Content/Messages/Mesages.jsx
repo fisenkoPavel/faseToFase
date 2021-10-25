dd6a091436e2404dff20b9c7440ca467dbb475b8
fisenkoPavel <fisenkopav@gmail.com>
2021-10-25 18:34:49 +0300

    Добавление props в Messages

--- src/Components/Content/Messages/Mesages.jsx
+++ src/Components/Content/Messages/Mesages.jsx
@@ -3,21 +3,37 @@ import React from 'react';
 import style from './Messages_style.module.css';
 import { NavLink } from 'react-router-dom';
 
-function Messages () {
+
+
+const DialogItem = (props) => {
+
+    let path = '/messages/' + props.id;
+    return (
+        <div className={style.item}><NavLink to={path} activeClassName={style.activeLink}>{props.name}</NavLink></div>
+    )
+};
+
+const Message = (props) => {
+    return (
+        <div className={style.message}>{props.message}</div>
+    )
+};
+
+const Messages = (props) => {
     return (
         <section className={style.app_messages}>
             <div className={style.dialogs}>
                 <div className={style.dialogsItems}>
-                    <div className={style.item}><NavLink to='/messages/1' activeClassName={style.activeLink}>Павел</NavLink></div>
-                    <div className={style.item}><NavLink to='/messages/2' activeClassName={style.activeLink}>Дмитрий</NavLink></div>
-                    <div className={style.item}><NavLink to='/messages/3' activeClassName={style.activeLink}>Алексей</NavLink></div>
-                    <div className={style.item}><NavLink to='/messages/4' activeClassName={style.activeLink}>Роман</NavLink></div>
+                    <DialogItem name='Павел' id='1'/>
+                    <DialogItem name='Дмитрий' id='2'/>
+                    <DialogItem name='Алексей' id='3'/>
+                    <DialogItem name='Роман' id='4'/>
                 </div>
             </div>
             <div className={style.messages}>
-                <div className={style.message}>Привет</div>
-                <div className={style.message}>Как дела??</div>
-                <div className={style.message}>Чем занимаешься??</div>
+                <Message message='Привет'/>
+                <Message message='Как дела??'/>
+                <Message message='Чем занимаешься??'/>
             </div>
         </section>
     )
