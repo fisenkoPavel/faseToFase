45a28097b136cdf8e2f6ccba58805cc40cb030d5
fisenkoPavel <fisenkopav@gmail.com>
2021-10-29 17:12:24 +0300

    onClick, ref, VirtualDOM

--- src/Components/Content/Profile/My_post/NewPost/NewPost.jsx
+++ src/Components/Content/Profile/My_post/NewPost/NewPost.jsx
@@ -3,8 +3,24 @@ import React from 'react';
 import style from './NewPostStyle.module.css';
 
 const NewPost = () => {
+
+    let newPostElement = React.createRef();
+
+    let addPost = () => {
+        let text = newPostElement.current.value;
+        alert(text);
+    }
+
     return (
         <div className={style.new_post}>
+            <div>
+                <div>
+                    <textarea ref={newPostElement}></textarea>
+                </div>
+                <div>
+                    <button onClick={ addPost }>Новый пост</button>
+                </div>
+            </div>
             <form action="">
                 <input type="text" />
                 <input type="submit" />
